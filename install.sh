889f0ef1256549f1e72bfc98809710ace9df188f
Edwin <developeredwin@gmail.com>
2019-11-21 14:12:44 -0600

    Instead of changing to the directory, use the bin directory for every conda command

--- install.sh
+++ install.sh
@@ -5,20 +5,23 @@ echo "Installing Anaconda distribution"
 wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh;
 bash Anaconda3-2019.10-Linux-x86_64.sh;
 
-# Change to the directory
-cd $HOME/anaconda3/bin/
-echo "Now in the $(pwd) directory."
+# Save the binary directory for later
+ANACONDABIN="$HOME/anaconda3/bin"
 
 # Create a new environment and activate it
 ANACONDAENV="toml";
 echo "Creating the $ANACONDAENV environment."
-./conda create -n $ANACONDAENV python=3
-./conda activate $ANACONDAENV
+$ANACONDABIN/conda create -n $ANACONDAENV python=3 --yes
+$ANACONDABIN/conda activate $ANACONDAENV
 
 # In the new environment, install a TOML parser
 pip install tomlkit
 
 # And run the configuration script
 python apply_configs.py
+
+# Finally, remove the environment
+$ANACONDABIN/conda remove -n $ANACONDAENV --all --yes
+echo "Removing the $ANACONDAENV environment."
 
 echo "DONE! :)"
