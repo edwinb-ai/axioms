0480c22667d00977b54e49b56744a1f543942298
Edwin <developeredwin@gmail.com>
2019-11-21 14:46:51 -0600

    Base programs are now automatically installed

--- apply_configs.py
+++ apply_configs.py
@@ -1,5 +1,12 @@
 import toml
+import subprocess as sbp
 
 
+# Load the configuration file and save it as a dictionary
 config_file = toml.load("master-config.toml")
-print(config_file)
+
+# ==== PROGRAMS ==== #
+basic_command = ["sudo", "eopkg", "it"]
+for k, v in config_file["programs"].items():
+    if k == "base":
+        sbp.run(basic_command + v)
