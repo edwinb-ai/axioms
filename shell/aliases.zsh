cd96ac3b3ef5b8f8666ba208d460bf9c8d97a4a0
Edwin <developeredwin@gmail.com>
2019-11-21 15:03:26 -0600

    Split aliases and cleaned up zshrc

---
+++ shell/aliases.zsh
@@ -0,0 +1,17 @@
+# ======== ALIASES =======
+# zsh
+alias zshconfig="code ~/.zshrc"
+alias zshup="source ~/.zshrc"
+
+# exa
+alias lgl="exa -l --git --header"
+alias lga="exa --grid --across --header --long"
+
+# server Sastre
+alias educk="ssh edwin@148.214.16.183"
+
+# ddgr
+alias bang="ddgr --gb --np"
+
+# Visual Studio Code
+alias code="code-oss"
---
+++ shell/zshrc.zsh
@@ -0,0 +1,46 @@
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME.oh-my-zsh"
+
+# fd-fzf exports
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Add Cargo (Rust package manager) to PATH
+export PATH=$HOME/.cargo/bin:$PATH
+
+# === THEME ===
+ZSH_THEME="spaceship"
+
+# Uncomment the following line to automatically update without prompting.
+DISABLE_UPDATE_PROMPT="true"
+
+# Uncomment the following line to enable command auto-correction.
+ENABLE_CORRECTION="true"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(z git zsh-syntax-highlighting)
+
+source $ZSH/oh-my-zsh.sh
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$("$HOME/anaconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
