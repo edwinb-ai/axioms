f11ebb3a9c2980fbc306c610bd9620eecbfa51ea
Edwin <developeredwin@gmail.com>
2019-11-22 14:32:05 -0600

    :boom:
    Breaking changes, should still work but languages cannot be yet installed

--- apply_configs.py
+++ apply_configs.py
@@ -19,7 +19,7 @@ def create_programs_dir(name: Optional[str] = "programs") -> None:
         pass
 
 
-def parse_special_programs(programs: List, command: List):
+def parse_with_url(programs: List, command: List):
     # First, create a special directory to store everything
     create_programs_dir()
     os.chdir(f"{os.getenv('HOME')}/programs")
@@ -34,7 +34,8 @@ def parse_special_programs(programs: List, command: List):
                 try:
                     sbp.run(tmp_list, check=True)
                 except sbp.CalledProcessError:
-                    print("Not available in repositories, trying with url")
+                    print("Not available in repositories or rejected by the user.")
+                    print("Trying with the URL.")
                     continue
             if k == "url":
                 if ".git" in v:
@@ -45,7 +46,7 @@ def parse_special_programs(programs: List, command: List):
                         sbp.run(tmp_list, check=True)
                         print("Done!")
                     except sbp.CalledProcessError:
-                        print("Could not clone the repository")
+                        print("Could not clone the repository.")
                 if "zip" in v:
                     r = requests.get(v)
                     if r.status_code == requests.codes.ok:
@@ -71,19 +72,50 @@ def parse_special_programs(programs: List, command: List):
 # Load the configuration file and save it as a dictionary
 config_file = toml.load("master-config.toml")
 
-# * PROGRAMS
+# Master command to install most things
 basic_command = ["sudo", "eopkg", "it"]
 
-# Start parsing the programs section
-for k, v in config_file["programs"].items():
-    # * Base programs
-    if k == "base":
-        # sbp.run(basic_command + v)
-        pass
-    # * Special programs
-    if k == "special":
-        print(v)
-        parse_special_programs(v, "sudo apt install".split(" "))
+# * Languages
+# TODO: Change to directory
+for m in config_file["languages"].values():
+    for i in m:
+        for k, v in i.items():
+            tmp_list = "sudo apt install".split(" ")
+            tmp_list.append(v)
+            if k == "name":
+                try:
+                    sbp.run(tmp_list, check=True)
+                except sbp.CalledProcessError:
+                    print("Not available in repositories or rejected by the user.")
+                    print("Trying with the URL.")
+                    continue
+            if k == "url":
+                if ".tar.gz" in v:
+                    r = requests.get(v, stream=True)
+                    if r.status_code == requests.codes.ok:
+                        print("Downloading the file...")
+                        print("Extracting the file...")
+                        with gzip.open(r.raw) as g:
+                            t = tarfile.TarFile(fileobj=g)
+                            t.extractall()
+                        print("Done!")
+                    else:
+                        raise requests.HTTPError("Could not download the file!")
+            if k == "script":
+                # TODO: Parse scripts correctly, not currently working
+                try:
+                    sbp.run(v.split(" "), check=True)
+                except sbp.CalledProcessError:
+                    print("Couldn't execute script.")
+
+# * PROGRAMS
+# for k, v in config_file["programs"].items():
+#     # * Base programs
+#     if k == "base":
+#         sbp.run(basic_command + v)
+#     # * Special programs
+#     if k == "special":
+#         parse_with_url(v, "sudo apt install".split(" "))
 
 # * Shell
 # Grab the oh-my-zsh installation script
@@ -92,4 +124,3 @@ for k, v in config_file["programs"].items():
 # )
 # # Execute it
 # sbp.run(["sh", "-c", omf_file.text])
-# TODO: Parse the TOML file once oh-my-fish has been installed
