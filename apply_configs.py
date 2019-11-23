1ec3784383896a55583227b4738a5b76705391cf
Edwin <developeredwin@gmail.com>
2019-11-22 23:51:26 -0600

    :sparkles:
    Brand new parsing for the shell, with basic configurations and aliases

--- apply_configs.py
+++ apply_configs.py
@@ -97,38 +97,42 @@ config_file = toml.load("master-config.toml")
 # Master command to install most things
 basic_command = ["sudo", "eopkg", "it"]
 
+# Save the axioms directory
+axioms_dir = os.getcwd()
+
 # First, create a special directory to store everything
 # and change the current directory to it
 create_programs_dir()
 os.chdir(f"{os.getenv('HOME')}/programs")
 
 # * Languages
-for m in config_file["languages"].values():
-    for i in m:
-        for k, v in i.items():
-            # Create a temporary list to store the full command
-            # tmp_list = basic_command.copy()
-            tmp_list = "sudo apt install".split(" ")
-            tmp_list.append(v)
+# for m in config_file["languages"].values():
+#     for i in m:
+#         for k, v in i.items():
+#             # Create a temporary list to store the full command
+#             tmp_list = basic_command.copy()
+#             # tmp_list = "sudo apt install".split(" ")
+#             tmp_list.append(v)
 
-            # Try first by name
-            if k == "name":
-                try:
-                    sbp.run(tmp_list, check=True)
-                except sbp.CalledProcessError:
-                    print("Not available in repositories or rejected by the user.")
-                    print("Trying with the URL.")
-                    continue
-            # If not, try by url
-            if k == "url":
-                download_and_decompress(v)
-            # Sometimes, these have scripts
-            if k == "script":
-                try:
-                    r = sbp.run(v, shell=True, check=True)
-                    print(r)
-                except sbp.CalledProcessError:
-                    print("Couldn't execute script.")
+#             # Try first by name
+#             if k == "name":
+#                 try:
+#                     sbp.run(tmp_list, check=True)
+#                 except sbp.CalledProcessError:
+#                     print("Not available in repositories or rejected by the user.")
+#                     print("Trying with the URL.")
+#                     continue
+
+#             # If not, try by url
+#             if k == "url":
+#                 download_and_decompress(v)
+#             # Sometimes, these have scripts
+#             if k == "script":
+#                 try:
+#                     r = sbp.run(v, shell=True, check=True)
+#                     print(r)
+#                 except sbp.CalledProcessError:
+#                     print("Couldn't execute script.")
 
 # * PROGRAMS
 # for k, v in config_file["programs"].items():
@@ -146,3 +150,16 @@ for m in config_file["languages"].values():
 # )
 # # Execute it
 # sbp.run(["sh", "-c", omf_file.text])
+# Rewrite zshrc
+zsh_location = f"{os.getenv('HOME')}/.testrc"
+with open(zsh_location, "w") as z:
+    for k, v in config_file["shell"].items():
+        if k == "name":
+            print(f"# This is a config file for the {v} shell.\n", file=z)
+        if k == "config":
+            print("# Global configuration file", file=z)
+            print(f"source {v}\n", file=z)
+        if k == "alias":
+            print("# Aliases file", file=z)
+            print(f"source {v}\n", file=z)
+
