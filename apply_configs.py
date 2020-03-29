803b8103c37e7308d72ac2e3db133a29b42773fc
edwinb-ai <developeredwin@gmail.com>
2020-03-29 12:44:56 -0600

    :boom:
    Removed some lines in the main parser and started working on
    the new software needed in the master configuration file.

--- apply_configs.py
+++ apply_configs.py
@@ -183,7 +183,8 @@ def git_configuration(config_file: Dict, axioms_dir: str) -> None:
 def parse_editor(config_file: Dict, axioms_dir: str) -> None:
     for k, v in config_file["editor"].items():
         # First, check to see if the editor is VSCode
-        destination = f"{os.getenv('HOME')}/.config/Code/User/"
+        #destination = f"{os.getenv('HOME')}/.config/Code/User/"
+        destination = f"{os.getenv('HOME')}/.config/Code - OSS/User/"
         if k == "name":
             if is_program := _check_if_program(v, destination, "Visual Studio Code"):
                 continue
@@ -197,7 +198,8 @@ def parse_editor(config_file: Dict, axioms_dir: str) -> None:
             # Look for the extensions section and loop over
             for m, n in ext_file["extensions"].items():
                 if m == "names":
-                    install_command = "code --install-extension".split(" ")
+                    #install_command = "code --install-extension".split(" ")
+                    install_command = "code-oss --install-extension".split(" ")
                     # The extensions are a list to loop over
                     for e in n:
                         tmp_list = install_command.copy()
