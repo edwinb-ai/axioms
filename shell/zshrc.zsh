8d369901c8b49a1fc85e26e95894d8067d9de238
edwinb-ai <developeredwin@gmail.com>
2020-10-03 19:06:26 -0500

    :bug:
    Still trying to sign the commits, some weird bug is still around.

--- shell/zshrc.zsh
+++ shell/zshrc.zsh
@@ -8,19 +8,18 @@ export FZF_DEFAULT_COMMAND='fdfind --type file --follow --hidden --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Add Cargo (Rust package manager) to PATH
-# export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME/.cargo/bin:$PATH
 
 # Export Julia binaries
 # export PATH=$HOME/programs/julia-1.5.1/bin:$PATH
 
 # Add poetry
-# export PATH=$HOME/.poetry/bin:$PATH
+export PATH=$HOME/.poetry/bin:$PATH
 
 # Add Go
 export PATH=$PATH:/usr/local/go/bin
 
 # ==== THEME ==== #
-# ZSH_THEME="spaceship"
 
 # Enable the starship prompt
 eval "$(starship init zsh)"
@@ -50,4 +49,4 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 # Add custom configuration
-export STARSHIP_CONFIG=~/.config/starship.toml
+export STARSHIP_CONFIG=$HOME/.config/starship.toml
